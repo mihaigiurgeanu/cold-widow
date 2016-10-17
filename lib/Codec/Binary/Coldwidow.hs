96a3a276d7e4163e8254434572e60d3d83615e47
Mihai Giurgeanu <Mihai.Giurgeanu@bcr.wan>
2016-10-17 16:20:11 +0300

    Implemented decode function.

--- lib/Codec/Binary/Coldwidow.hs
+++ lib/Codec/Binary/Coldwidow.hs
@@ -4,7 +4,8 @@ import Data.Bits (shiftL, (.|.))
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as B
 import Data.Word (Word8)
-import Numeric (showIntAtBase)
+import Numeric (showIntAtBase, readInt)
+import Data.List (elemIndex)
 
 
 chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
@@ -23,7 +24,10 @@ packInteger :: ByteString -> Integer
 packInteger s = packInteger' (B.unpack s) 0
 
 decode :: String -> Integer
-decode _ = 0
+decode encoded = let parsed = readEncodedInt encoded
+                 in case parsed of
+                      [(val, [])] -> val
+                      otherwise -> error "Error parsing encoded value"
 
 unpackInteger :: Integer -> ByteString
 unpackInteger = undefined
@@ -32,6 +36,18 @@ unpackInteger = undefined
 
 toDigit :: Int -> Char 
 toDigit i = chars !! i -- TODO: make a more efficient implementation
+
+readEncodedInt :: ReadS Integer
+readEncodedInt = readInt base isDigit fromDigit
+
+isDigit :: Char -> Bool
+isDigit digit = digit >= '0' && digit <= ':'
+
+fromDigit :: Char -> Int
+fromDigit digit = let val = elemIndex digit chars
+                  in case val of
+                       Nothing -> error ("Decoding error: character "++[digit]++" is not in the alphabet")
+                       Just val' -> val'
 
 packInteger' :: [Word8] -> Integer -> Integer
 packInteger' [] result = result
