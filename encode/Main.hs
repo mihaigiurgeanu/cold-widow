f1d15d20e3e258d1d7366f5147dc1c6a8c934a74
Mihai Giurgeanu <Mihai.Giurgeanu@bcr.wan>
2016-10-17 13:37:57 +0300

    Extracted generic functions into cold-widow lib.

--- encode/Main.hs
+++ encode/Main.hs
@@ -1,12 +1,9 @@
 module Main where
 
 import System.Environment (getArgs)
-import Data.ByteString.Lazy (ByteString)
-import Data.Bits (shiftL, (.|.))
-import Data.Word (Word8)
-import Numeric (showIntAtBase)
-
 import qualified Data.ByteString.Lazy as B
+
+import Codec.Binary.Coldwidow
 
 main :: IO ()
 main = do 
@@ -15,25 +12,6 @@ main = do
   bigval <- readFileAsInteger fileName
   putStr $ encode bigval
 
-chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-         'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-         'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '$', '%', '*',
-         '+', '-', '.', '/', ':']
-
-base = 45
-
-encode :: Integer -> String
-encode x = showIntAtBase base toDigit x ""
-
-toDigit i = chars !! i -- TODO: make a more efficient implementation
-
 readFileAsInteger :: FilePath -> IO Integer
 readFileAsInteger fileName = B.readFile fileName  >>= return . packInteger
 
-packInteger :: ByteString -> Integer
-packInteger s = packInteger' (B.unpack s) 0
-
-packInteger' :: [Word8] -> Integer -> Integer
-packInteger' [] result = result
-packInteger' (d:ds) result = packInteger' ds ((fromIntegral d) .|. (result `shiftL` 8))
---
+++ lib/Codec/Binary/Coldwidow.hs
@@ -0,0 +1,36 @@
+module Codec.Binary.Coldwidow (encode, decode, packInteger) where
+
+import Data.Bits (shiftL, (.|.))
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as B
+import Data.Word (Word8)
+import Numeric (showIntAtBase)
+
+
+chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+         'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+         'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+         'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '$', '%', '*',
+         '+', '-', '.', '/', ':']
+
+base = 45
+
+-- external inteface
+encode :: Integer -> String
+encode x = showIntAtBase base toDigit x ""
+
+packInteger :: ByteString -> Integer
+packInteger s = packInteger' (B.unpack s) 0
+
+decode :: String -> Integer
+decode _ = 0
+
+-- internal functions
+
+toDigit :: Int -> Char 
+toDigit i = chars !! i -- TODO: make a more efficient implementation
+
+packInteger' :: [Word8] -> Integer -> Integer
+packInteger' [] result = result
+packInteger' (d:ds) result = packInteger' ds ((fromIntegral d) .|. (result `shiftL` 8))
+
