3469c1ac9a45b3cec7b251ee11c3080374343c31
Mihai Giurgeanu <mihaigiurgeanu@MacBook-Mihai.local>
2016-11-13 19:32:01 +0200

    Defined options structure for make-codes command.

--- cold-widow/Main.hs
+++ cold-widow/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import System.Console.Program
 import System.Console.Command
 import System.Console.Argument 
+import Control.Applicative ((<$))
 
 main :: IO ()
 main = single program
@@ -22,3 +23,30 @@ program = Node (command "cold-widow" "Transfer files via QRCodes" . io $ interac
 
 makeCodesAction = io $ putStrLn "make-codes not implemented"
 receiveAction = io $ putStrLn "receive not implemented"
+
+
+singleStepOption = option ['1'] ["fast", "single-step"] boolean False "Force single step execution, in memory"
+tempFileOption = option ['t'] ["temporary-file"] boolean False "Force single step execution using a temporary file"
+blockSizeOption = option ['s'] ["bs", "block-size"] natural 0 "Use the specified block size instead of calculating the optimal blocksize"
+qrVersionOption = option ['q'] ["qr-version"]  version AutoVersion "Use the specified qr code version instead of automatically determine the version"
+levelOption = option ['l'] ["error-level"] errorLevel AutoLevel "Use the specified error-level instead of automatically determine the optimal level"
+
+data VersionOption = Version Int | AutoVersion
+data LevelOption = LevelL | LevelM | LevelQ | LevelH | AutoLevel
+
+version :: Type VersionOption
+version = Type (parseVersionOption . parser natural) "QR Code Version" Nothing
+
+parseVersionOption (Right x) | x >= 1 && x <= 40 = Right . Version $ fromInteger x
+parseVersionOption (Right x) | otherwise = Left "QR code version must be between 1 and 40"
+parseVersionOption (Left x) = Left x
+
+errorLevel :: Type LevelOption
+errorLevel = Type parseLevelOption "QR Code Error correction level" Nothing
+
+parseLevelOption "L" = Right LevelL
+parseLevelOption "M" = Right LevelM
+parseLevelOption "Q" = Right LevelQ
+parseLevelOption "H" = Right LevelH
+parseLevelOption _ = Left "Only L, M, Q or H levels are accepted for error-level argument"
+
